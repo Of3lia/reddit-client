6bb427ce40ad53be95b0fa4d753a6ab627a0fcbc
Of3lia <ofeliacocucci@outlook.com>
2021-12-08 17:35:30 +0100

    Adds video

--- src/components/RedditListItem.vue
+++ src/components/RedditListItem.vue
@@ -8,12 +8,17 @@
     <v-row>
       <v-col>
         <v-img
+          v-if="!post.is_video"
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           :src="post.thumbnail"
           transition="scale-transition"
         />
+        <video v-else width="100%" controls>
+          <source :src="post.secure_media.reddit_video.fallback_url" />
+          Your browser does not support the video tag.
+        </video>
       </v-col>
     </v-row>
   </v-card>

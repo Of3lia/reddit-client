667720e5a4e944b07cbd642ad71ba7357896a6dc
Of3lia <ofeliacocucci@outlook.com>
2021-12-08 08:46:48 +0100

    adds axios, gets posts from api

---
+++ src/components/RedditListContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ posts }}</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "RedditListContainer",
+  created() {
+    this.$store.dispatch("loadPosts");
+  },
+  computed: mapState(["posts"]),
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: [],
   },
   mutations: {
+    SET_POSTS(state, posts) {
+      state.posts = posts;
+      console.log(posts);
+    }
   },
   actions: {
+    loadPosts({ commit }) {
+      axios
+        .get('https://www.reddit.com/r/aww/.json?after=t3_raw188&limit=1')
+        .then(r => r.data)
+        .then(posts => {
+          commit('SET_POSTS', posts)
+        }).catch(e => {
+          console.log(e);
+          alert('Ups! There was an error, try again later');
+        });
+    }
   },
   modules: {
   }
